971f2f12952e1b954f904b8bc6dd364e8c158e8a
Jessica Gladstone <jcg321@comcast.net>
2018-09-22 00:14:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+ return document.querySelector(selector);
 
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div');
+}
+
+function increaseRankBy(n) {
+   const rank = document.getElementById('app').querySelectorAll('.ranked-list');
+  for (let i=0; i < rank.length; i ++) {
+	  rank[i].innerHTML = (i + n).toString();
+  }
+}
